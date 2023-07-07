ec91fb6689bf1c1d46001a04cdec49dbcaa3de77
Ngawang P Sherpa <ngawangpsherpa@gmail.com>
2023-07-07 15:55:54 -0400

    final commit

---
+++ Todo/src/TodoList.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+
+function TodoList() {
+  const [todoItems, setTodoItems] = useState(0);
+
+  const addTodo = (todo) => {
+    setTodoItems(todoItems.concat(todo0));
+  };
+  const removeTodo = (index) => {
+    const updatedTodos = todoItems.splice();
+    updatedTodos.splice(index, 1);
+    setTodoItems(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const todoINput = e.target.elements.todo;
+          addTodo({
+            text: todoInput.value,
+            completed: false,
+          });
+          todoINput.value = "";
+        }}
+      >
+        <input type="text" name="todo" placeholder="Enter a new todo" />
+        <button type="submit">Add ToDo</button>
+      </form>
+      <ul>
+        {todoItems.map((todo, index) => (
+          <li key={index}>
+            {todo.text}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
